768d3ed90a152862947d3286bf11ea338d905569
felipejsm <felipejsm41@gmail.com>
2025-09-28 18:50:41 -0300

    chore: developing controller w/ tdd

--- src/test/java/com/pocapi/poc/controller/UserControllerTest.java
+++ src/test/java/com/pocapi/poc/controller/UserControllerTest.java
@@ -1,9 +1,19 @@
 package com.pocapi.poc.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pocapi.poc.model.UserMetadata;
 import com.pocapi.poc.service.UserClientService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class UserControllerTest {
 
     @Mock
@@ -11,8 +21,31 @@ public class UserControllerTest {
 
     @Mock
     UserController controller;
-    @Test
-    void shouldCallUserControllerAndGetAllUserMetadata() {
 
+    String jsonData = """
+            {
+              "page": 0,
+              "per_page": 0,
+              "total": 0,
+              "total_pages": 0,
+              "data": [
+                {
+                  "id": 0,
+                  "email": "string",
+                  "first_name": "string",
+                  "last_name": "string",
+                  "avatar": "string"
+                }
+              ]
+            }
+            """;
+    ObjectMapper objectMapper;
+    @Test
+    void shouldCallUserControllerAndGetAllUserMetadata() throws JsonProcessingException {
+        objectMapper = new ObjectMapper();
+        UserMetadata user = objectMapper.readValue(jsonData, UserMetadata.class);
+        when(service.getUsers()).thenReturn(user);
+        ResponseEntity response = controller.getAll();
+        assertEquals(ResponseEntity.ok(), response.getStatusCode());
     }
 }
