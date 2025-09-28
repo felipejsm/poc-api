50ff6cac0782d0b4c88ba753358895d625e62eca
felipejsm <felipejsm41@gmail.com>
2025-09-28 18:44:30 -0300

    chore(tdd): adding controller test

---
+++ src/test/java/com/pocapi/poc/controller/UserController.java
@@ -0,0 +1,9 @@
+package com.pocapi.poc.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1")
+public class UserController {
+}
---
+++ src/test/java/com/pocapi/poc/controller/UserControllerTest.java
@@ -0,0 +1,18 @@
+package com.pocapi.poc.controller;
+
+import com.pocapi.poc.service.UserClientService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+public class UserControllerTest {
+
+    @Mock
+    UserClientService service;
+
+    @Mock
+    UserController controller;
+    @Test
+    void shouldCallUserControllerAndGetAllUserMetadata() {
+
+    }
+}
